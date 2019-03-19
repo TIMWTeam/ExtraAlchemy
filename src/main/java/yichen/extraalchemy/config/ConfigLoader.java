216f53a7055c9edd471bcc8fdd9535d3d5833fbf
Taskeren <nitu2003@126.com>
2019-03-20 00:01:32 +0800

    重写 ConfigLoader

--- src/main/java/yichen/extraalchemy/config/ConfigLoader.java
+++ src/main/java/yichen/extraalchemy/config/ConfigLoader.java
@@ -18,6 +18,7 @@ public class ConfigLoader {
 		config = new Configuration(event.getSuggestedConfigurationFile());
 		config.load();
 		load();
+		config.save();
 		
 		
 	}
@@ -25,13 +26,11 @@ public class ConfigLoader {
 	{
 		logger.info("Started loading config.\n开始配置文件加载 ");
 
-		comment = "Alchemy array basic work efficiency. [range: 0.1 ~ 5.0, default: 1.0] \n炼金法阵基础工作效率。[范围：0.1 ~ 5.0，默认：1.0]";
-		alchemyArrayEfficiency = (float) config.get(Configuration.CATEGORY_GENERAL, "alchemyArrayEfficiency", 1.0, comment, 0.1, 5.0).getDouble();
+		comment = "Alchemy array basic work efficiency. [range: 0.1 ~ 5.0, default: 1.0]\n炼金法阵基础工作效率。[范围：0.1 ~ 5.0，默认：1.0]";
+		alchemyArrayEfficiency = config.getFloat("alchemyArrayEfficiency", Configuration.CATEGORY_GENERAL, 1.0f, 0.1f, 5.0f, comment);
 
-		comment = "Blood bottle drinking speed.  \n血瓶饮用速度。";
+		comment = "Blood bottle drinking speed.\n血瓶饮用速度。";
 		bloodBottleDrinking = config.getInt("bloodBottleDrinking", Configuration.CATEGORY_GENERAL, 20, 1, 60, comment, "bloodBottleDrinking");
-
-		config.save();
 		
 		logger.info("Finished loading config.\n完成配置文件加载 ");
 		
