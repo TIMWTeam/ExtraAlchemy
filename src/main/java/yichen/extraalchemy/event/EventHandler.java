becdc0e8d11528ff2717f65ed966a5acb99ccdbe
YiChen_ <1183233617@qq.com>
2019-03-22 19:43:27 +0800

    贴图入库

---
+++ src/main/java/yichen/extraalchemy/event/EventHandler.java
@@ -0,0 +1,12 @@
+package yichen.extraalchemy.event;
+
+import net.minecraftforge.event.world.BlockEvent.PortalSpawnEvent;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+public class EventHandler {
+
+	@SubscribeEvent
+	public static void onPortalSpawnEvent(PortalSpawnEvent event) {
+		event.setCanceled(false);
+	}
+}
